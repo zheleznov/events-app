45e6333d4f4131fe00a632f66760a8ca3d30e622
Рома Железнов < >
2016-02-04 12:51:32 +0200

    add password validation

--- js/app.js
+++ js/app.js
@@ -15,6 +15,9 @@
             init: function(){
                 this.users.init();
             },
+            getFromStorage: function(){
+
+            },
             users: {
                 init: function() {
                     if(!localStorage.users) {
@@ -75,15 +78,30 @@
                     $('.signup-form').hide();
                     $('.signup-form input').val('');
                 }
+            },
+            signUp: {
+                hideAlert: function(param, result){
+                    $('body').find('.alert-danger p[data-password="' + param + '"]').hide();
+
+                    if (result) $('body').find('.alert-danger').hide();
+                },
+                showAlert: function(param, result){
+                    $('body').find('.alert-danger p[data-password="' + param + '"]').show();
+                    if (result) $('body').find('.alert-danger').show();
+                }
             }
         };
 
+
+        //TODO: refactoring
+        //TODO: check email for existing user
         var controllers = {
             init: function(){
                 this.users.setEvents();
                 this.users.checkUserSession();
                 this.pages.setEvents();
                 this.pages.setPage();
+                this.signUp.setEvents();
             },
             users: {
                 login: function(e){
@@ -144,6 +162,71 @@
                 setEvents: function(){
                     $('.nav-signup').on('click', this.signUp);
                     $('.navbar-brand').on('click', this.mainPage);
+                }
+            },
+            signUp: {
+                validateEmail: function(){
+                    var password = $(this).val(),
+                        finalResult = false;
+
+                    //uppercase
+                    if(password.match(/[A-Z]/g)){
+                        renders.signUp.hideAlert('uppercase', finalResult);
+                        finalResult = true;
+                    } else {
+                        renders.signUp.showAlert('uppercase', finalResult);
+                        finalResult = false;
+                    }
+
+                    //lowercase
+                    if(password.match(/[a-z]/g)){
+                        renders.signUp.hideAlert('lowercase', finalResult);
+                        finalResult = true;
+                    } else {
+                        renders.signUp.showAlert('lowercase', finalResult);
+                        finalResult = false;
+                    }
+
+                    //digit
+                    if(password.match(/\d/g)){
+                        renders.signUp.hideAlert('digit', finalResult);
+                        finalResult = true;
+                    } else {
+                        renders.signUp.showAlert('digit', finalResult);
+                        finalResult = false;
+                    }
+
+                    //length
+                    if(password.length > 6 ){
+                        renders.signUp.hideAlert('length', finalResult);
+                        finalResult = true;
+                    } else {
+                        renders.signUp.showAlert('length', finalResult);
+                        finalResult = false;
+                    }
+
+                    this.isPasswordValid = finalResult;
+                },
+                createNewUser: function(){
+                    var userInfo = {};
+
+                    userInfo.name = $('body').find('#name').val();
+                    userInfo.lastName = $('body').find('#lastName').val();
+                    userInfo.email = $('body').find('#email').val();
+                    userInfo.password = $('body').find('#password').val();
+                    userInfo.employer = $('body').find('#employer').val();
+                    userInfo.job = $('body').find('#job').val();
+                    userInfo.birth = $('body').find('#birth').val();
+                    console.log(this)
+                    console.log(this.isPasswordValid)
+                    if(userInfo.name && userInfo.lastName && userInfo.email && this.isPasswordValid) {
+                        alert(2)
+                        storages.users.addNewUser(userInfo);
+                    }
+                },
+                setEvents: function(){
+                    $('body').on('keyup', '#password', this.validateEmail.bind(this));
+                    $('body').on('click', '.signup-form .btn-success', this.createNewUser.bind(this));
                 }
             }
         };
